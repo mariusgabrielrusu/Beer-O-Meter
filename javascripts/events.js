825a943a847d8e723c192260df3fd9dec4814a45
marius.rusu <marius.rusu@zitec.com>
2015-09-24 18:10:39 +0300

    first commit

---
+++ javascripts/events.js
@@ -0,0 +1,8 @@
+// $(document).on('tap', '#next-to-data', function(e) {
+//   e.preventDefault();
+//   $.mobile.changePage($('#data'));
+// });
+
+$(document).on('tap', '#beers', function(e) {
+  console.log($('#beers').val());
+})
